d47c97d4d3acad3e91545109c8f070ff1112e415
Natasha Draper <natasha@draper.net.za>
2022-06-13 08:22:04 +0200

    Hopefully fix memory leak

--- client/src/app/services/update.service.ts
+++ client/src/app/services/update.service.ts
@@ -1,6 +1,6 @@
 import { ApplicationRef, Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-import { concat, interval, map, Observable, startWith } from 'rxjs';
+import { concat, first, interval, map, Observable, startWith } from 'rxjs';
 import { environment } from "../../environments/environment";
 
 @Injectable({
@@ -11,7 +11,7 @@ export class UpdateService {
 
   constructor(private readonly swUpdate: SwUpdate, private readonly appRef: ApplicationRef) {
     if (environment.production && location.protocol.startsWith('https')) {
-      const appIsStable$ = appRef.isStable.pipe(map((stable) => stable));
+      const appIsStable$ = appRef.isStable.pipe(first((stable) => stable));
       const every30Seconds$ = interval(30 * 1000);
       const every30SecondsWhenAppIsStable$ = concat(appIsStable$, every30Seconds$);
       every30SecondsWhenAppIsStable$.subscribe(() => swUpdate.checkForUpdate());
